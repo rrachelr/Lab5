75d259852fa21092fd3075bc85e926a37c38a4e5
rrachelr <rachrobins1@gmail.com>
2022-04-04 19:17:23 -0400

    Update PieceBuzz.java
    Did all the same updating i did in PieceBlueHen in PieceBuzz

--- Lab5/src/main/java/PieceBuzz.java
+++ Lab5/src/main/java/PieceBuzz.java
@@ -1,11 +1,7 @@
-public class PieceBuzz {
-    private char symbol;
-    private String teamColor;
+public class PieceBuzz extends Piece {
     private int numAttacks;
     private int numTimesBeenAttacked;
     private boolean workingLaser;
-    private boolean hidden;
-    private boolean original;
 
     public PieceBuzz(char symbol,
                       String teamColor,
@@ -14,13 +10,10 @@ public class PieceBuzz {
                       boolean workingLaser,
                       boolean hidden,
                       boolean original) {
-        this.symbol = symbol;
-        this.teamColor = teamColor;
+        super(symbol, teamColor, hidden, original);
         this.numAttacks = numAttacks;
         this.numTimesBeenAttacked = numTimesBeenAttacked;
         this.workingLaser = workingLaser;
-        this.hidden = hidden;
-        this.original = original;
     }
 
     public PieceBuzz(){
@@ -29,12 +22,6 @@ public class PieceBuzz {
                 true, false, true);
     }
 
-    public char getSymbol() {
-        return symbol;
-    }
-    public String getTeamColor() {
-        return teamColor;
-    }
     public int getNumAttacks() {
         return numAttacks;
     }
@@ -44,25 +31,9 @@ public class PieceBuzz {
     public boolean canAttack(){
         return workingLaser;
     }
-    public boolean isHidden() {
-        return hidden;
-    }
-    public boolean isOriginal(){ return original;}
 
-    public void setSymbol(char symbol) {
-        this.symbol = symbol;
-    }
-    public void setTeamColor(String teamColor) {
-        this.teamColor = teamColor;
-    }
     public void setWorkingLaser(boolean workingLaser) {
         this.workingLaser = workingLaser;
-    }
-    public void setHidden(boolean hidden) {
-        this.hidden = hidden;
-    }
-    public void setOriginal(boolean original) {
-       this.original = original; 
     }
 
     public void setNumAttacks(int numAttacks)  {
@@ -78,12 +49,13 @@ public class PieceBuzz {
         System.out.println("To Infinity and Beyond!");
     }
 
-    public boolean validMovePath(int fromSquareRow, int fromSquareCol,
+    /* public boolean validMovePath(int fromSquareRow, int fromSquareCol,
                                  int toSquareRow, int toSquareCol) {
         // You will implement this method in a later step
         // each Piece will have a different valid path
         return true;
-    }
+    } */
+
     public Piece spawn(){
         return null;
     }

9cb429b16e29eb295435e8a5a8fae9a9b15fba96
rrachelr <rachrobins1@gmail.com>
2022-04-04 19:19:30 -0400

    Update PieceMinion.java
    Updating again for PieceMinion this time

--- Lab5/src/main/java/PieceMinion.java
+++ Lab5/src/main/java/PieceMinion.java
@@ -1,22 +1,15 @@
-public class PieceMinion {
-    private char symbol;
-    private String teamColor;
+public class PieceMinion extends Piece {
     private int numRecruits;
     private int numTimesSpawned;
-    private boolean hidden;
-    private boolean original;
 
     public static int MAX_NUM_SPAWNED = 3;
 
     public PieceMinion(char symbol, String teamColor,
                         int numRecruits, int numTimesSpawned,
                         boolean hidden, boolean original) {
-        this.symbol = symbol;
-        this.teamColor = teamColor;
+        super(symbol, teamColor, hidden, original);
         this.numRecruits = numRecruits;
         this.numTimesSpawned = numTimesSpawned;
-        this.hidden = hidden;
-        this.original = original;
     }
 
     public PieceMinion(){
@@ -25,30 +18,11 @@ public class PieceMinion {
                 false,true);
     }
 
-    public char getSymbol() {
-        return symbol;
-    }
-    public String getTeamColor() {
-        return teamColor;
-    }
     public int getNumRecruits() {
         return numRecruits;
     }
     public int getNumTimesSpawned() {
         return numTimesSpawned;
-    }
-    public boolean isHidden() {
-        return hidden;
-    }
-    public boolean isOriginal() {
-        return original;
-    }
-   
-    public void setHidden(boolean hidden) {
-        this.hidden = hidden;
-    }
-    public void setOriginal(boolean original){
-      this.original = original; 
     }
 
     public void setNumRecruits(int numRecruits) {
@@ -59,12 +33,12 @@ public class PieceMinion {
         System.out.println("Bello!");
     }
 
-    public boolean validMovePath(int fromSquareRow, int fromSquareCol,
+   /* public boolean validMovePath(int fromSquareRow, int fromSquareCol,
                                  int toSquareRow, int toSquareCol) {
         // You will implement this method in a later step
         // each Piece will have a different valid path
         return true;
-    }
+    } */
 
     public PieceMinion spawn(){
         return new PieceMinion(Character.toLowerCase(this.symbol),

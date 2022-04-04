c3ae39888eb733e1d591655ed67d84c48ea281ed
rrachelr <rachrobins1@gmail.com>
2022-04-04 18:34:51 -0400

    Update Piece.java
    Added a Piece constructor

--- Lab5/src/main/java/Piece.java
+++ Lab5/src/main/java/Piece.java
@@ -3,4 +3,11 @@ public abstract class Piece {
     protected String teamColor;
     protected boolean hidden;
     protected boolean original;
+
+    public Piece(char symbol, String teamColor, boolean hidden, boolean original) {
+        this.symbol = symbol;
+        this.teamColor = teamColor;
+        this.hidden = hidden;
+        this.original = original;
+    }
 }

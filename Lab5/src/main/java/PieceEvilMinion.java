7b89c9247624e9b6da0bb5aae212f01ef556132b
rrachelr <rachrobins1@gmail.com>
2022-04-05 13:17:10 -0400

    Update PieceEvilMinion.java
    Removed hungry from the constructor after reading instructions again, added empty constructor

--- Lab5/src/main/java/PieceEvilMinion.java
+++ Lab5/src/main/java/PieceEvilMinion.java
@@ -2,9 +2,14 @@ public class PieceEvilMinion extends PieceMinion1{
     private int numAttacks;
     private boolean hungry;
 
-    public PieceEvilMinion (char symbol, String teamColor, int numRecruits, int numAttacks, int numTimesSpawned, boolean hidden, boolean original, boolean hungry) {
+    final public int MAX_NUM_ATTACKS = 4;
+
+    public PieceEvilMinion (char symbol, String teamColor, int numRecruits, int numAttacks, int numTimesSpawned, boolean hidden, boolean original) {
         super(symbol, teamColor, numRecruits, numTimesSpawned, hidden, original);
         this.numAttacks = numAttacks;
-        this.hungry = hungry;
+    }
+
+    public PieceEvilMinion () {
+        this('E', "NON", 0, 0, 0, false, true);
     }
 }

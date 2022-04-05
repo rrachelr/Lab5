965271bf8aca23c0a7ef3831b354ba9457f5641c
rrachelr <rachrobins1@gmail.com>
2022-04-05 13:54:16 -0400

    Revert "Refactoring PieceEvilMinion"
    This reverts commit c3fe871cf55f67735a1350dc7ee0d2b876924305.

--- Lab5/src/main/java/PieceEvilMinion.java
+++ Lab5/src/main/java/PieceEvilMinion.java
@@ -1,16 +1,16 @@
-public class PieceEvilMinion1 extends PieceMinion1{
+public class PieceEvilMinion extends PieceMinion1{
     private int numAttacks;
     private boolean hungry;
 
     final public int MAX_NUM_ATTACKS = 4;
 
-    public PieceEvilMinion1 (char symbol, String teamColor, int numRecruits, int numAttacks, int numTimesSpawned, boolean hidden, boolean original) {
+    public PieceEvilMinion (char symbol, String teamColor, int numRecruits, int numAttacks, int numTimesSpawned, boolean hidden, boolean original) {
         super(symbol, teamColor, numRecruits, numTimesSpawned, hidden, original);
         this.numAttacks = numAttacks;
         updateHungry();
     }
 
-    public PieceEvilMinion1 () {
+    public PieceEvilMinion () {
         this('E', "NON", 0, 0, 0, false, true);
     }
 
@@ -46,8 +46,8 @@ public class PieceEvilMinion1 extends PieceMinion1{
         return true;
     }
 
-    public PieceEvilMinion1 spawn() {
-        PieceEvilMinion1 babyMinion = new PieceEvilMinion1(Character.toLowerCase(this.symbol), this.teamColor, 1, 0 , 0, false, false);
+    public PieceEvilMinion spawn() {
+        PieceEvilMinion babyMinion = new PieceEvilMinion(Character.toLowerCase(this.symbol), this.teamColor, 1, 0 , 0, false, false);
         babyMinion.hungry = true;
         babyMinion.setNumTimesSpawned(this.numTimesSpawned + 1);
         return babyMinion;
